dc3b1d80d2b6f0aa9470eeb6c338a266b8d1138d
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2020-09-20 18:43:30 +0200

    first commit

---
+++ LaboAPI/NetworkService.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkServiceDelegate.swift
+//  LaboAPI
+//
+//  Created by Benjamin on 16/09/2020.
+//  Copyright © 2020 Benjamin. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkServiceDelegate {
+    func getData()
+}
---
+++ LaboAPI/Views/TableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewController.swift
+//  LaboAPI
+//
+//  Created by Benjamin on 16/09/2020.
+//  Copyright © 2020 Benjamin. All rights reserved.
+//
+
+import Foundation
